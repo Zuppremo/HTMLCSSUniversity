8587f306a8c982277b67266749453762f03466a4
Deybi Montenegro (Zuppremo) <deibymontenegrob@gmail.com>
2023-05-18 00:27:11 -0500

    removed bad project

--- Uninpahu/ParkingLotMotorbikeUninpahu/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ParkingLotMotorbikeUninpahu.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
