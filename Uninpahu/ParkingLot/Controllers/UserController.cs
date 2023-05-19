929c1de65430a7dc625ae10152e5d7ece71cbf81
Deybi Montenegro <deibymontenegrob@gmail.com>
2023-05-18 23:35:32 -0500

    new views

---
+++ Uninpahu/ParkingLot/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParkingLot.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
