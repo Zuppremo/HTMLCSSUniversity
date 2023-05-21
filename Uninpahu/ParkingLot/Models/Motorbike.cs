fb0128856b051d04a1f3202961c3e7e5a7f0ade4
Deybi Montenegro (Zuppremo) <deibymontenegrob@gmail.com>
2023-05-21 13:55:07 -0500

    modify some models

--- Uninpahu/ParkingLot/Models/Motorbike.cs
+++ Uninpahu/ParkingLot/Models/Motorbike.cs
@@ -2,9 +2,9 @@
 {
     public class Motorbike
     {
-        public int IdMotorbike { get; set;}
-        public required string MotorbikePlate { get; set; }
-        public int MotorbikeModel { get; set; }
+        public int Id { get; set;}
+        public required string Plate { get; set; }
+        public int Model { get; set; }
         public int IdMotorbikeBrand { get; set; }
     }
 }
--- Uninpahu/ParkingLot/Models/MotorbikeBrand.cs
+++ Uninpahu/ParkingLot/Models/MotorbikeBrand.cs
@@ -2,7 +2,7 @@
 {
     public class MotorbikeBrand
     {
-        public int IdMotorbikeBrand { get; set; }
-        public required string MotorbikeBrandName { get; set; }
+        public int Id { get; set; }
+        public required string Name { get; set; }
     }
 }
